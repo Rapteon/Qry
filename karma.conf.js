2be42efc94194acc1d8c98dd00dc18467121a1d4
Rapteon <rapteon@tutanota.com>
2022-11-12 18:23:45 +0530

    Renamed app from qr-code-generator to qry.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/qr-code-generator'),
+      dir: require('path').join(__dirname, './coverage/qry'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'qr-code-generator'`, () => {
+  it(`should have as title 'qry'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('qr-code-generator');
+    expect(app.title).toEqual('qry');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('qr-code-generator app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('qry app is running!');
   });
 });
